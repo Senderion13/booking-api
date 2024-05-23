6534aee10862e6fe3fb8d97ff8174e15f3d21712
BelikovArtem <airubus.tochka@gmail.com>
2024-05-23 11:01:51 +0300

    Added Filter Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FindRoomsModule } from './find-rooms/find-rooms.module';
 
 @Module({
-  imports: [],
+  imports: [FindRoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
