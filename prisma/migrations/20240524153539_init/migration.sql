02b4d6768f20169cbe3dcd9ed98e310ff9ce96f7
Senderion13 <79333316+Senderion13@users.noreply.github.com>
2024-05-28 01:06:05 -0700

    created migration for prisma (#5)

---
+++ prisma/migrations/20240524153539_init/migration.sql
@@ -0,0 +1,66 @@
+-- CreateTable
+CREATE TABLE "hotels" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "hotels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "hotel_images" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "hotelId" INTEGER NOT NULL,
+
+    CONSTRAINT "hotel_images_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rooms" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "hotelId" INTEGER,
+    "price" INTEGER NOT NULL,
+    "guests" INTEGER NOT NULL,
+    "hasWifi" BOOLEAN NOT NULL,
+    "hasParking" BOOLEAN NOT NULL,
+    "hasBar" BOOLEAN NOT NULL,
+    "hasShower" BOOLEAN NOT NULL,
+    "hasBreakfast" BOOLEAN NOT NULL,
+
+    CONSTRAINT "rooms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "room_images" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "roomId" INTEGER NOT NULL,
+
+    CONSTRAINT "room_images_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "hotel_images" ADD CONSTRAINT "hotel_images_hotelId_fkey" FOREIGN KEY ("hotelId") REFERENCES "hotels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rooms" ADD CONSTRAINT "rooms_hotelId_fkey" FOREIGN KEY ("hotelId") REFERENCES "hotels"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "room_images" ADD CONSTRAINT "room_images_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "rooms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FindRoomsModule } from './find-rooms/find-rooms.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FindRoomsModule, ServeStaticModule.forRoot({}), PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FindRoomsModule, ServeStaticModule.forRoot({})],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
