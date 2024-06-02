fb735ad1ffcb7df8dfcbf06e41497a7339c6e631
BelikovArtem <airubus.tochka@gmail.com>
2024-06-02 12:38:35 +0300

    Added Hotel Image URL to responce

---
+++ src/find-hotels/entities/hotel.entity.ts
@@ -0,0 +1,10 @@
+export default class Hotel {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  title: string;
+  rating: number;
+  location: string;
+  description: string;
+  imageUrl: string;
+}
--- src/find-hotels/find-hotels.service.ts
+++ src/find-hotels/find-hotels.service.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import Hotel from './entities/hotel.entity';
 import FindHotelsDTO from './dto/FindHotels.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FindHotelsService {
@@ -9,21 +10,52 @@ export class FindHotelsService {
   /*
    * Return all hotels
    */
-  async findAll() {
-    return await this.prisma.hotel.findMany();
+  async findAll(): Promise<Hotel[]> {
+    const hotels = await this.prisma.hotel.findMany();
+    return await this.addImagesToHotels(hotels);
   }
 
   /*
    * Return all rooms that match the conditions
    */
-  async filterHotels(query: FindHotelsDTO) {
+  async filterHotels(query: FindHotelsDTO): Promise<Hotel[]> {
     try {
-      return await this.prisma.hotel.findMany(
+      const hotels = await this.prisma.hotel.findMany(
         this.getFilterParamsFromQuery(query),
       );
+      if (hotels.length === 0) {
+        return [];
+      } else {
+        return await this.addImagesToHotels(hotels);
+      }
     } catch {
       throw new BadRequestException('Incorrect parameters');
     }
+  }
+
+  async getFirstHotelImage(hotelId: number) {
+    return await this.prisma.hotelImages.findFirst({
+      select: { url: true },
+      where: {
+        hotelId: hotelId,
+      },
+    });
+  }
+
+  async addImagesToHotels(hotels): Promise<Hotel[]> {
+    const hotelsWithImages: Hotel[] = [];
+    for (let i = 0; i < hotels.length; i++) {
+      // get first hotel image associated with hotel id
+      const hotelImage = await this.getFirstHotelImage(hotels[i].id);
+
+      // map results from DataBase to Hotel entity
+      hotelsWithImages[i] = new Hotel();
+      for (const key in hotels[i]) {
+        hotelsWithImages[i][key] = hotels[i][key];
+      }
+      hotelsWithImages[i].imageUrl = hotelImage.url;
+    }
+    return hotelsWithImages;
   }
 
   getFilterParamsFromQuery(query: FindHotelsDTO) {
