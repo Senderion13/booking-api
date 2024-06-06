8710bcdab343322e6f395f537f3d2dd1c2eb6257
Senderion13 <79333316+Senderion13@users.noreply.github.com>
2024-06-06 03:13:24 -0700

    Dev (#12)
    * implemented Room class, server functions marked as async
    
    * Implemented find-rooms module with Prisma (database ORM) integration
    
    * Fix: min/maxPrice parameters now working
    
    * Added DTO for query validation
    
    * Chekpoint version. (Works with npx prisma migrate command each time you start a server on docker)
    
    * Added Hotels Module/Controller/Service
    
    * Bao/hotel info with images (#9)
    
    * Added Hotel Image URL to responce
    
    * Added rooms images to responce.
    
    * Deleted "localhost:5000/" from image urls in seed ts. Migration complete
    
    * changed seeds with migration (#10)
    
    * cors fixed
    
    * updated migration installation and set work mode to production
    
    * added images path for production
    
    ---------
    
    Co-authored-by: BelikovArtem <airubus.tochka@gmail.com>
    Co-authored-by: BelikovArtem <148897755+BelikovArtem@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { FindRoomsModule } from './find-rooms/find-rooms.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { FindHotelsModule } from './find-hotels/find-hotels.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     FindRoomsModule,
-    ServeStaticModule.forRoot({}),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client'),
+    }),
     FindHotelsModule,
     PrismaModule,
   ],
